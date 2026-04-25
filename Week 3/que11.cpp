44bebb253140dd88030303c34a7674efb0337952
Nirbhay925 <nt3667426@gmail.com>
2026-04-25 18:59:21 +0530

    Add duplicate detection in array program
    Implement a program to check for duplicates in an array.

---
+++ Week 3/que11.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        bool found = false;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == a[i - 1]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
